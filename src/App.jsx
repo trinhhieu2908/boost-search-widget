89bf85ef1383e84e3ee9ade7fcb72f451db68857
TrinhHieu <hieu.trantrinh@orientsoftware>
2023-07-14 16:26:18 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./configs/configureStore";
-
-
 import WidgetPage from "./pages/widgetPage";
 import Header from "./components/Header/Header";
 import SearchWidgetSetting from "./auto-suggestions-search-widget/SearchWidgetSetting/SearchWidgetSetting";
